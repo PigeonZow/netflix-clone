ce9e6494e0743bfaf8bdce87ae5a7e0a31a3eaa1
Patrick Zhao <patrick_zhao@sfu.ca>
2022-05-21 02:36:52 -0700

    arrow show error

--- pages/index.tsx
+++ pages/index.tsx
@@ -65,7 +65,7 @@ const Home: NextPage<Props> = (props) => {
       <Header />
       <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16"> 
         <Banner netflixOriginals={props.netflixOriginals}/>
-        <section className="md:space-y-24">
+        <section className="md:space-y-24 ">
           <Row title="Trending Now" movies={props.trendingNow} />
           <Row title="Top Rated" movies={props.topRated} />
           <Row title="Action Thrillers" movies={props.actionMovies} />
